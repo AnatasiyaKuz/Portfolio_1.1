b142a11749557c68883c60c502946153ea66ac92
Anastasiya <nastya.kuzich.02@mail.ru>
2023-11-06 19:45:10 +0200

    added project block

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import About from "./components/About/About";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
+import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       </div>
       <About/>
       <Skills/>
+      <Projects/>
     </div>
   );
 }
